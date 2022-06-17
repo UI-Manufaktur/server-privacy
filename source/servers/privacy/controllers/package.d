929f6f017e340bf3ba3085fab36c328a9071ea96
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-17 06:26:07 +0200

    Update package.d

--- source/servers/privacy/controllers/package.d
+++ source/servers/privacy/controllers/package.d
@@ -1,1 +1,5 @@
+module servers.privacy.controllers;
 
+public import servers.privacy.controllers.actions;
+public import servers.privacy.controllers.api;
+public import servers.privacy.controllers.pages;
